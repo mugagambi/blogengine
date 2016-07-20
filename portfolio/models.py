a2f10cc989f15e9559578f840d3b81d54ebcf12d
Humphrey Mugambi <mugagambi@gmail.com>
2016-07-20 18:23:53 +0300

    Added imag field in my portfolio

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Portofolios(models.Model):
-    img = None
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    images = models.ImageField(upload_to='images')
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
 
 # Create your models here.
